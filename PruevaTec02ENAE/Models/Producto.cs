7f8d8a2013a6c48b9df205e6f5a58c674bfe2bee
Emerson719 <ae24001@esfe.agape.edu.sv>
2024-02-21 10:16:03 -0600

    Modelos y DbContext Creados  Usando Data First

---
+++ PruevaTec02ENAE/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PruevaTec02ENAE.Models
+{
+    public partial class Producto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public decimal Precio { get; set; }
+        public string? Descripcion { get; set; }
+        public byte[]? Imagen { get; set; }
+        public int? CategoriaId { get; set; }
+
+        public virtual Categoria? Categoria { get; set; }
+    }
+}
---
+++ PruevaTec02ENAE/Models/Producto2Context.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PruevaTec02ENAE.Models
+{
+    public partial class Producto2Context : DbContext
+    {
+        public Producto2Context()
+        {
+        }
+
+        public Producto2Context(DbContextOptions<Producto2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categorias { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=localhost; database=Producto2; integrated security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.Property(e => e.CategoriaId).HasColumnName("Categoria_id");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.Property(e => e.ProductoId).HasColumnName("Producto_id");
+
+                entity.Property(e => e.CategoriaId).HasColumnName("Categoria_id");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Precio).HasColumnType("decimal(9, 2)");
+
+                entity.HasOne(d => d.Categoria)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.CategoriaId)
+                    .HasConstraintName("FK__Productos__Categ__398D8EEE");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
